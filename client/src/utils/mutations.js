8fbdb723d933343dfa0bb443c0eb3ae023ff8435
lydiawdesign <lydia.williamson.OSU@gmail.com>
2022-01-06 08:38:05 -0500

    refactoring dependencies

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -19,6 +19,16 @@ export const ADD_USER = gql`
       user {
         _id
         username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
       }
     }
   }
